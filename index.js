c4b25bfc08a5e7e3359355895dc24566f0e31d70
Dor <dorzairi@ymail.com>
2021-02-26 17:43:27 -0500

    Making it work

---
+++ index.js
@@ -0,0 +1,33 @@
+let currentNumber;
+let result = 1;
+
+let H = document.getElementById("h1");
+let Plus = document.getElementById("plus");
+let Minus = document.getElementById("minus");
+let Number = document.getElementById("number");
+
+Plus.addEventListener("click", plus);
+Minus.addEventListener("click", minus);
+
+currentNumber = parseInt(Number.value);
+
+function plus(click) {
+  //do something that add the currentNumber to the input number
+
+  currentNumber = parseInt(Number.value);
+  result += currentNumber;
+  render();
+}
+
+function minus(click) {
+  //do something that add the currentNumber to the input number
+  currentNumber = parseInt(Number.value);
+  result -= currentNumber;
+  render();
+}
+
+function render() {
+  H.innerHTML = result;
+}
+
+render();
